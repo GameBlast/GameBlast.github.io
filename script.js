34aa2bbe0a941d5cc2b78be671d345248595f498
GameBlast <comunicacao@gameblast.com.br>
2026-03-22 13:50:38 -0300

    No dropdown de meses, deixa o texto somente em minúsculo

--- script.js
+++ script.js
@@ -250,7 +250,7 @@ function criaDropdowns() {
       if (moment().month() == i) {
          dropMes.insertAdjacentHTML(
             'beforeend',
-            `<option value='${i}' selected='true'>${meses[i]}</option>`
+            `<option value='${i}' selected='true'>${meses[i].toLowerCase()}</option>`
          );
       } else {
          dropMes.insertAdjacentHTML(
